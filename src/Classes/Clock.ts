8aa178cb09396f7b3484c95fa2813c3018541293
kemani young <5429440ky@gmail.com>
2020-12-30 10:01:46 -0500

    Mentor Help

--- src/Classes/Clock.ts
+++ src/Classes/Clock.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs';
+
  export class Clock{
     private  CurrentDateTime:Date; 
     private diffDays:number
@@ -14,80 +16,114 @@
         this.diffSec=0;
     }
 //Displays Data calulated 
-    public  TimeLaps(DateTimeEntered:Date ){
-        
-            //Resets the current Date object-For Accurate comparision 
-            this.CurrentDateTime=new Date();
-            //Subtract the for the differnce of the two Date Object 
-            let diff=(this.CurrentDateTime.getTime()-DateTimeEntered.getTime());
-           
-            //The following if statement validate the "diff's" millseconds passed is eqivalinet to Week  Day  Hours  Mins Sec
-            //Week 
-            if(diff>=(1000 * 3600 * 24 * 7)){
-                this.diffWeeks= Math.trunc(diff/ (1000 * 3600 * 24*7));
-                diff=((diff)% (1000 * 3600 * 24 * 7));
-            }
-            //Day
-            if(diff>=(1000 * 3600 * 24)){
-                this.diffDays=Math.trunc (diff/ (1000 * 3600 * 24));
-                diff=((diff)% (1000 * 3600 * 24));
-            }
-            //Hours
-            if(diff>=(1000 * 3600)){
-                this.diffHours= Math.trunc((diff)/ (1000 * 3600));
-                diff=((diff)% (1000 * 3600 ));
-            }
-            //Mins
-            if(diff>=(1000 * 60)){
-                this.diffMins= Math.trunc((diff)/ (1000 * 60));
-                diff=((diff)% (1000 * 60));
-            }
-            //Sec
-            if(diff>=(1000)){
-                this.diffSec= Math.trunc((diff)/ (1000));
-                diff=((diff)% (1000));
-            }
-       
-        //Returns date passed in a defualt format
-        return this.diffWeeks+" Weeks "+this.diffDays+" Days "+this.diffHours+" Hours "+this.diffMins+" Mins "+this.diffSec+" Sec "  
-        
+    public  TimeLaps(DateTimeEntered:Date ): Observable<string>{
+        return new Observable(subscriber=>{
+            setInterval(()=>{
+                //Resets the current Date object-For Accurate comparision 
+                this.CurrentDateTime=new Date();
+                //Subtract the for the differnce of the two Date Object 
+                let diff=(this.CurrentDateTime.getTime()-DateTimeEntered.getTime());
+
+                //The following if statement validate the "diff's" millseconds passed is eqivalinet to 
+                // Week  Day  Hours  Mins Sec
+
+                //Week 
+                if(diff>=(1000 * 3600 * 24 * 7)){
+                    this.diffWeeks= Math.trunc(diff/ (1000 * 3600 * 24*7));
+                    diff=((diff)% (1000 * 3600 * 24 * 7));
+                }
+                //Day
+                if(diff>=(1000 * 3600 * 24)){
+                    this.diffDays=Math.trunc (diff/ (1000 * 3600 * 24));
+                    diff=((diff)% (1000 * 3600 * 24));
+                }
+                //Hours
+                if(diff>=(1000 * 3600)){
+                    this.diffHours= Math.trunc((diff)/ (1000 * 3600));
+                    diff=((diff)% (1000 * 3600 ));
+                }
+                //Mins
+                if(diff>=(1000 * 60)){
+                    this.diffMins= Math.trunc((diff)/ (1000 * 60));
+                    diff=((diff)% (1000 * 60));
+                }
+                //Sec
+                if(diff>=(1000)){
+                    this.diffSec= Math.trunc((diff)/ (1000));
+                    diff=((diff)% (1000));
+                }
+
+                //Returns date passed in a defualt format
+                let result =  this.diffWeeks+" Weeks "+this.diffDays+" Days "+this.diffHours+" Hours "+this.diffMins+" Mins "+this.diffSec+" Sec "  
+                subscriber.next(result)
+            },1000)
+        })
     }
-//Function used to format Current Date 
-//Retunring yy/mm/dd
-    public CurrentDate(format:string){
-        //if Satement used to sort format bassed on the parameter passed
-        if(format=="DD-MM-YY"){
-            let d=this.CurrentDateTime=new Date();
-            return d.toDateString()
-        }
-        else if(format=="MAX"){
-            let Y =this.CurrentDateTime.getFullYear();
-            let M= this.CurrentDateTime.getMonth()+1;
-            let D= this.CurrentDateTime.getDate();
-            return (Y+"-"+M+"-"+(D-1));
-        }
-        return " ";
+    
+    /**
+     * Function used to format Current Date 
+     * Retunring yy/mm/dd
+     **/
+    public CurrentDate(format:string): Observable<string>{
+        return new Observable(subscriber=>{
+            setInterval(()=>{
+                //if Satement used to sort format bassed on the parameter passed
+                if(format=="DD-MM-YY"){
+                    let d=this.CurrentDateTime=new Date();
+                    subscriber.next(d.toDateString())
+                }
+            },1000)
+        })
+       
+    }
+
+    public maxDate(): string{
+        let Y =this.CurrentDateTime.getFullYear();
+        let M= this.CurrentDateTime.getMonth()+1;
+        let D= this.CurrentDateTime.getDate();
+        return (Y+"-"+M+"-"+(D-1));
     }
     
     //Function used to format and return  Current Date 
-    public CurrentTime(){
-        //Resets current Date
-        this.CurrentDateTime=new Date();
-        let Minutes
-        //If used to fromat Time
-        if(this.CurrentDateTime.getMinutes()<10){
-            Minutes="0"+this.CurrentDateTime.getMinutes()
-        }
-        else{
-            Minutes=this.CurrentDateTime.getMinutes();
-        }
+    public CurrentTime(): Observable<string>{
+        return new Observable(subscriber=>{
+            setInterval(()=>{
+                //Resets current Date
+                this.CurrentDateTime=new Date();
+                let mode:  string = "AM";
+                let hours: string;
+                let Minutes: string = this.CurrentDateTime.getMinutes().toString();
+                let seconds:string = this.CurrentDateTime.getSeconds().toString();
 
-        if(this.CurrentDateTime.getHours()>12){
+                // We need to give the correct hour
+                if(this.CurrentDateTime.getHours() > 12)//Then we are in the PM
+                {
+                    hours = (this.CurrentDateTime.getHours() - 12).toString(); //then we can get hours 
+                    mode = "PM"
+                }else if(this.CurrentDateTime.getHours() == 12){
+                    hours = '12';
+                    mode = "PM"
+                }else if(this.CurrentDateTime.getHours() == 0){ // then is 12 midnight 
+                    hours = '12';
+                }else{//then its after 12 AM
+                    hours = (this.CurrentDateTime.getHours()).toString()
+                    
+                }
 
-            return ((this.CurrentDateTime.getHours()-12)+":"+Minutes+":"+this.CurrentDateTime.getSeconds()+" PM ");
-        }else{
-            return (this.CurrentDateTime.getHours()+":"+Minutes+":"+this.CurrentDateTime.getSeconds()+" AM ");
-        }
+                // this will only be true when the value is less than 10
+                if(this.CurrentDateTime.getSeconds()<10){
+                    seconds ="0"+this.CurrentDateTime.getSeconds();
+                }
+                //If used to fromat Time
+                if(this.CurrentDateTime.getMinutes()<10){
+                    Minutes="0"+this.CurrentDateTime.getMinutes()
+                }
+                
+                subscriber.next(`${hours}:${Minutes}:${seconds} ${mode}`);
+                
+            })
+        })
+        
     }
     //Getters 
     public getWeeks(){
--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,5 @@
-import {Clock} from './Classes/Clock.js'
+import { Clock } from './Classes'
+
 
 const form=document.querySelector("#form") as HTMLFormElement;
 const DateHtml=document.querySelector("#startDate") as HTMLInputElement;
@@ -13,29 +14,34 @@ let Loop:any;
 
 window.addEventListener("load",(e:Event)=>{
     e.preventDefault();
-    setInterval(()=>{
-
-        CTime.innerHTML= ClockObj.CurrentTime();
-        CDate.innerHTML=ClockObj.CurrentDate("DD-MM-YY");
-    },1000);
+    
+    ClockObj.CurrentTime().subscribe((result:string)=>{
+        CTime.innerHTML= result;
+    })
+    
+    ClockObj.CurrentDate("DD-MM-YY").subscribe((res:string)=>{
+        CDate.innerHTML=res;
+    })
 
 });
 form.addEventListener("mouseover",(e:Event)=>{
     e.preventDefault();
     //Calls a method that returns the max date from the current time, setting the max attribute    
-    DateHtml.setAttribute('max',ClockObj.CurrentDate("MAX")); 
+    DateHtml.setAttribute('max',ClockObj.maxDate()); 
     
 });
 
 //Button event listerner used to display out 
 form.addEventListener('submit',(e:Event)=>{
     e.preventDefault();
-
-    function timePasedFun(){
-        //Pass output from timelaps function which  accepts a Date Object which also accepts the value from both Datepicker and TimePicker Input. 
-        TimePassed.innerHTML=ClockObj.TimeLaps(new Date(DateHtml.value+" "+Time.value));
-    }
-    Loop=setInterval(timePasedFun,1000);
+    //Pass output from timelaps function which  accepts a Date Object which also accepts the value from both Datepicker and TimePicker Input. 
+    ClockObj.TimeLaps(new Date(DateHtml.value+" "+Time.value)).subscribe((res:string)=>{
+        TimePassed.innerHTML=res
+    });
+    
+    // no longer need this function as the class is handling the looping and updating of the result
+    // function timePasedFun(){}
+    // Loop=setInterval(timePasedFun,1000); 
     
 });
 //Date input event listerner used to resest displayed output
